a2e697fd44166c00b2d83f595cb887d805d9ef0e
Andrii <boltenkovandrii@gmail.com>
2026-09-23 16:52:09 +0200

    More debug experiments

--- resources/scripts/test_run.ps1
+++ resources/scripts/test_run.ps1
@@ -13,7 +13,6 @@ Remove-Item reports/allure-report -Recurse -Force
 #pytest   -n 8 --browser chromium --profile=desktop_2560x1600  --profile=tablet --profile=mobile --profile=tablet_landscape --profile=mobile_landscape src/tests/prestashop/test_localization.py
 #pytest   -n 8 --browser firefox --browser chromium --browser webkit --profile=desktop_1920x1200 src/tests/prestashop/test_account.py
 #pytest   -n 8 --browser chromium --profile=desktop_1920x1200 src/tests/prestashop/test_account.py
-#pytest  --headed  -n auto --browser chromium --profile=desktop_2560x1600 src/tests/prestashop/test_catalog_page.py
 #pytest  --headed  -n auto --browser chromium --profile=desktop_1920x1200
 #pytest  --headed  -n auto --browser chromium --profile=tablet_landscape --profile=mobile_landscape
 #pytest  -n 8 --browser chromium --profile=tablet --profile=mobile --profile=desktop_1920x1200 src/tests/prestashop/test_localization.py
